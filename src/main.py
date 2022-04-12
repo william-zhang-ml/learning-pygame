60c8b8a5be7f96159fb668e0b363c2605e06a343
william-zhang-ml <william.zhang.ml@gmail.com>
2022-04-11 21:28:18 -0400

    make linter happy

--- src/main.py
+++ src/main.py
@@ -14,19 +14,23 @@ class Game:
     """ RPG game engine. """
     def __init__(self) -> None:
         """ Constructor. Setup. """
+        # pylint: disable=no-member
         pygame.init()
+        # pylint: enable=no-member
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
 
     def run(self) -> None:
+        """ Start game engine. """
+        # pylint: disable=no-member
         events = pygame.event.get()
-        while all([e.type != pygame.QUIT for e in events]):
+        while all(e.type != pygame.QUIT for e in events):
             self.screen.fill('black')
             pygame.display.update()
             self.clock.tick(FPS)
             events = pygame.event.get()
-
         pygame.quit()
+        # pylint: enable=no-member
         sys.exit()
 
 
